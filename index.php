33a6f7da1ba08d95b14010db3869b91b5d932a2c
hamada-syusaku <99862542+hamada-syusaku@users.noreply.github.com>
2022-02-17 15:37:27 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Daily report</title>
+		<link href="../css/style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<header><p id="header">Daily report</p></header>
+		<div id="header2">
+			<a href="judge-insert.php">新規会員登録はコチラ</a>
+		</div>
+			<div class="image">
+			<img src="../images/img.jpg" with="100"height="150">
+			</div>
+		<div id="main-body">
+			<div id="topic1">
+				<p>ログイン</p>
+			</div>
+		<div id="main">
+			<div id="aaa">
+				<form action="uplode.php" method="post">
+						<label for="email">Email</label>
+						<div id="p">
+							<input type="text" id="email" name="email" style="width: 200px">
+						</div>
+						<label for="password">Password</label>
+						<div id="p">
+							<input type="password" id="password" name="password" style="width: 200px">
+						</div>
+						<div id="p">
+							<input type="submit" style="width: 50px">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<div id="sub-body">
+	<div id="aaa">
+			<div id="topic">
+			<p>Daily reportとは？</p>
+			</div>
+			</div>
+			<div id="sub_">
+						<p>Daily reportは日々の活動を記録する日報です。</p>
+			</div>
+	<div id="topic">
+		<p>オススメ機能①</p>
+	</div>
+	<div id="sub_">
+				<p>過去日報の検索機能</p>
+				<p>日報を登録する際、自己評価を５段階でつけていただきます。  <br>１〜５の自己評価を指定して過去の日報を表示できるのでユーザーの課題抽出をアシストします。</p>
+	</div>
+	<div id="topic">
+				<p>オススメ機能②</p>
+	</div>
+	<div id="sub_">
+				<p>担当者のコメント機能</p>
+				<p>担当者（上司、コーチ等..）は、指定ユーザーにコメントすることができます。<br>指定ユーザの進捗を把握、迅速なフィードバックをサポートします。</p>
+	</div>
+	</ul>
+</div>
+</form>
+	<footer>
+		<p id="footer">Daily report</p>
+		<p id="footer2">お問い合わせ:00-0000-0000<br>受付時間:平日  10時～17時  ※祝日・年末年始除く</p>
+	</footer>
+	</body>
+</html>
